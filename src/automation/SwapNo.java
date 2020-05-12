6890bdca09fbf054821a7a1a00fb2a54467a62da
Prakash-Sonawane <Prakash-Sonawane@PF1RJX2D.na.odcorp.net>
2020-05-12 14:56:48 +0530

    swap

---
+++ src/automation/SwapNo.java
@@ -0,0 +1,28 @@
+package automation;
+
+public class SwapNo {
+
+	public static void main(String[] args)
+	
+	
+	{
+		int a=10;
+		int b=20;
+		int temp ;
+		
+		// TODO Auto-generated method stub
+
+	/*	a=b+a;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);*/
+		
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println(b);
+	}
+
+}
